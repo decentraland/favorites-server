1b43ce9930791e489d428eef2ecff6c4e9baad72
Lautaro Petaccio <lausuper@gmail.com>
2023-03-31 12:32:58 -0300

    fix: Set count as 0 when converting an empty array of DBGetPicks

--- test/unit/lists-adapters.spec.ts
+++ test/unit/lists-adapters.spec.ts
@@ -1,49 +1,59 @@
-import {
-  fromDBGetPickByListIdToPickIdsWithCount,
-  fromDBPickToPick,
-  PickIdsWithCount
-} from '../../src/adapters/lists'
+import { fromDBGetPickByListIdToPickIdsWithCount, fromDBPickToPick, PickIdsWithCount } from '../../src/adapters/lists'
 import { DBGetFilteredPicksWithCount, DBPick } from '../../src/ports/picks'
 
 describe('when transforming DB retrieved picks to pick ids with count', () => {
   let dbGetPicksByListId: DBGetFilteredPicksWithCount[]
   let picksWithCount: PickIdsWithCount
 
-  beforeEach(() => {
-    const createdAt = new Date()
-    dbGetPicksByListId = [
-      {
-        item_id: '1',
-        user_address: '0x45abb534BD927284F84b03d43f33dF0E5C91C21f',
-        list_id: 'e96df126-f5bf-4311-94d8-6e261f368bb2',
-        created_at: createdAt,
-        picks_count: 3
-      },
-      {
-        item_id: '11',
-        user_address: '0x45abb534BD927284F84b03d43f33dF0E5C91C21f',
-        list_id: 'e96df126-f5bf-4311-94d8-6e261f368bb2',
-        created_at: createdAt,
-        picks_count: 3
-      },
-      {
-        item_id: '111',
-        user_address: '0x45abb534BD927284F84b03d43f33dF0E5C91C21f',
-        list_id: 'e96df126-f5bf-4311-94d8-6e261f368bb2',
-        created_at: createdAt,
-        picks_count: 3
+  describe('and there are not DB retrieved picks', () => {
+    beforeEach(() => {
+      dbGetPicksByListId = []
+      picksWithCount = {
+        picks: [],
+        count: 0
       }
-    ]
-    picksWithCount = {
-      picks: [{ itemId: '1' }, { itemId: '11' }, { itemId: '111' }],
-      count: 3
-    }
+    })
+
+    it('should return the transformed picks with count', () => {
+      expect(fromDBGetPickByListIdToPickIdsWithCount(dbGetPicksByListId)).toEqual(picksWithCount)
+    })
   })
 
-  it('should return the transformed picks with count', () => {
-    expect(fromDBGetPickByListIdToPickIdsWithCount(dbGetPicksByListId)).toEqual(
-      picksWithCount
-    )
+  describe('and there are multiple DB retrieved picks', () => {
+    beforeEach(() => {
+      const createdAt = new Date()
+      dbGetPicksByListId = [
+        {
+          item_id: '1',
+          user_address: '0x45abb534BD927284F84b03d43f33dF0E5C91C21f',
+          list_id: 'e96df126-f5bf-4311-94d8-6e261f368bb2',
+          created_at: createdAt,
+          picks_count: 3
+        },
+        {
+          item_id: '11',
+          user_address: '0x45abb534BD927284F84b03d43f33dF0E5C91C21f',
+          list_id: 'e96df126-f5bf-4311-94d8-6e261f368bb2',
+          created_at: createdAt,
+          picks_count: 3
+        },
+        {
+          item_id: '111',
+          user_address: '0x45abb534BD927284F84b03d43f33dF0E5C91C21f',
+          list_id: 'e96df126-f5bf-4311-94d8-6e261f368bb2',
+          created_at: createdAt,
+          picks_count: 3
+        }
+      ]
+      picksWithCount = {
+        picks: [{ itemId: '1' }, { itemId: '11' }, { itemId: '111' }],
+        count: 3
+      }
+    })
+
+    it('should return the transformed picks with count', () => {
+      expect(fromDBGetPickByListIdToPickIdsWithCount(dbGetPicksByListId)).toEqual(picksWithCount)
+    })
   })
 })
 
