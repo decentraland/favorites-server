b61bf32e51e380e0efa64ed57d4c6378429fc215
Kevin Szuchet <kevin.szuchet@decentraland.org>
2023-05-23 12:08:34 +0200

    test: Test cases in which access utils should not throw errors

--- test/unit/access-utils.spec.ts
+++ test/unit/access-utils.spec.ts
@@ -14,6 +14,14 @@ describe('when validating if an access exists', () => {
       )
     })
   })
+
+  describe('and the query returns something', () => {
+    it('should do nothing', () => {
+      expect(() => validateAccessExists(listId, permission, grantee, { rowCount: 5 })).not.toThrowError(
+        new AccessNotFoundError(listId, permission, grantee)
+      )
+    })
+  })
 })
 
 describe('when validating if an access is being duplicated', () => {
@@ -22,6 +30,14 @@ describe('when validating if an access is being duplicated', () => {
       expect(() =>
         validateDuplicatedAccess(listId, permission, grantee, { constraint: 'list_id_permissions_grantee_primary_key' })
       ).toThrowError(new DuplicatedAccessError(listId, permission, grantee))
+    })
+  })
+
+  describe('and the error does not have has the constraint of a duplicated primary key', () => {
+    it('should not throw the error', () => {
+      expect(() => validateDuplicatedAccess(listId, permission, grantee, {})).not.toThrowError(
+        new DuplicatedAccessError(listId, permission, grantee)
+      )
     })
   })
 })
