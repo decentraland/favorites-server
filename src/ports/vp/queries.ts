34cab8606ad34036ce7dca29358ef26a675d8946
Kevin Szuchet <kevin.szuchet@decentraland.org>
2023-06-01 09:14:21 +0200

    refactor: Check if the VP is undefined to do nothing on conflict

--- src/ports/vp/queries.ts
+++ src/ports/vp/queries.ts
@@ -1,15 +1,11 @@
 import SQL from 'sql-template-strings'
 
 export function insertVPQuery(power: number | undefined, userAddress: string) {
-  const query = SQL`INSERT INTO favorites.voting (user_address, power) `
+  const query = SQL`INSERT INTO favorites.voting (user_address, power) VALUES (${userAddress}, ${power ?? 0}) ON CONFLICT (user_address) `
 
   // If the snapshot query fails, the power will be undefined
   // Try to set the VP to 0 without overwriting it if it already exists
-  if (!power) {
-    query.append(SQL`VALUES (${userAddress}, ${0}) ON CONFLICT (user_address) DO NOTHING`)
-  } else {
-    query.append(SQL`VALUES (${userAddress}, ${power}) ON CONFLICT (user_address) DO UPDATE SET power = ${power}`)
-  }
+  query.append(typeof power === 'undefined' ? 'DO NOTHING' : SQL`DO UPDATE SET power = ${power}`)
 
   return query
 }
--- test/unit/vp-queries.spec.ts
+++ test/unit/vp-queries.spec.ts
@@ -1,7 +1,6 @@
 import { insertVPQuery } from '../../src/ports/vp/queries'
 
 let userAddress: string
-let power: number | undefined
 
 beforeEach(() => {
   userAddress = 'user-address'
@@ -10,7 +9,7 @@ beforeEach(() => {
 describe('when getting the insert VP query', () => {
   describe('and the power query is rejected', () => {
     it('should return a query to try to set the VP to 0 without overwriting it if it already exists', () => {
-      const query = insertVPQuery(power, userAddress)
+      const query = insertVPQuery(undefined, userAddress)
 
       expect(query.text).toContain('VALUES ($1, $2) ON CONFLICT (user_address) DO NOTHING')
       expect(query.values).toEqual(expect.arrayContaining([userAddress, 0]))
@@ -18,15 +17,13 @@ describe('when getting the insert VP query', () => {
   })
 
   describe('and the power query succeeds', () => {
-    beforeEach(() => {
-      power = 100
-    })
+    describe.each([0, 100])('and the VP got from snapshot is %d', (power: number) => {
+      it('should return a query to try to set the VP to the value returned by the query and updating it if already exists', () => {
+        const query = insertVPQuery(power, userAddress)
 
-    it('should return a query to try to set the VP to the value returned by the query and updating it if already exists', () => {
-      const query = insertVPQuery(power, userAddress)
-
-      expect(query.text).toContain('VALUES ($1, $2) ON CONFLICT (user_address) DO UPDATE SET power = $3')
-      expect(query.values).toEqual(expect.arrayContaining([userAddress, 100, 100]))
+        expect(query.text).toContain('VALUES ($1, $2) ON CONFLICT (user_address) DO UPDATE SET power = $3')
+        expect(query.values).toEqual(expect.arrayContaining([userAddress, power, power]))
+      })
     })
   })
 })
